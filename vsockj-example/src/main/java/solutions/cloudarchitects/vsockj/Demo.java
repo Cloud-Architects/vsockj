11a714207a940a32ff823f1e38ff37e0abceefda
Patryk Orwat <patrykorwat@gmail.com>
2020-12-01 22:09:36 +0800

    try default CID in aws-enclave

--- vsockj-example/src/main/java/solutions/cloudarchitects/vsockj/Demo.java
+++ vsockj-example/src/main/java/solutions/cloudarchitects/vsockj/Demo.java
@@ -3,12 +3,10 @@ package solutions.cloudarchitects.vsockj;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-import static solutions.cloudarchitects.vsockj.VSockAddress.VMADDR_CID_HOST;
-
 public class Demo {
 
     public static void main(String[] args) throws IOException {
-        VSock sock = new VSock(new VSockAddress(VMADDR_CID_HOST, 5000));
+        VSock sock = new VSock(new VSockAddress(10, 5000));
         sock.getOutputStream().write("Hello world".getBytes(StandardCharsets.UTF_8));
         sock.close();
     }
