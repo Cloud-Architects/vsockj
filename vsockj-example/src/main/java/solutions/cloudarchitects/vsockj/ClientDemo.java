a8129106a6792dc6be7f5704a302ec7443f77991
Patryk Orwat <patrykorwat@gmail.com>
2020-12-03 00:48:23 +0800

    simplified client demo code

--- vsockj-example/src/main/java/solutions/cloudarchitects/vsockj/ClientDemo.java
+++ vsockj-example/src/main/java/solutions/cloudarchitects/vsockj/ClientDemo.java
@@ -1,20 +1,17 @@
 package solutions.cloudarchitects.vsockj;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
-public class Demo {
+public class ClientDemo {
 
     public static void main(String[] args) throws IOException {
         VSock sock = new VSock(new VSockAddress(10, 5000));
         sock.getOutputStream()
                 .write("Hello world\n".getBytes(StandardCharsets.UTF_8));
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(sock.getInputStream()))) {
-            String line = reader.readLine();
-            System.out.println(line);
-        }
+        byte[] b = new byte[4096];
+        sock.getInputStream().read(b, 0, 4096);
+        System.out.println(new String(b, StandardCharsets.UTF_8));
         sock.close();
     }
 }
